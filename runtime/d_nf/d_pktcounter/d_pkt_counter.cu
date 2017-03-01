91f065fed46b7395fe900c6f2baaaba21c7be788
xiaodongyi <409070169@qq.com>
2017-03-01 13:37:04 +0800

    try add .cu and .ch

--- runtime/d_nf/d_pktcounter/d_pkt_counter.cu
+++ runtime/d_nf/d_pktcounter/d_pkt_counter.cu
@@ -8,7 +8,6 @@
 // For the CUDA runtime routines (prefixed with "cuda_")
 #include <cuda_runtime.h>
 
-#include <helper_cuda.h>
 
 bool d_registered_pkt_counter =
     d_static_nf_register::get_register().register_nf<d_pkt_counter, d_pkt_counter_fs>("pkt_counter", 1);
