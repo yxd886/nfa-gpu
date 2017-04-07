c3dee988baf7af3e3b1a9372f59f704200d08f40
xiaodongyi <409070169@qq.com>
2017-04-07 11:31:42 +0800

    add test

--- runtime/module/forward_ec_scheduler.cc
+++ runtime/module/forward_ec_scheduler.cc
@@ -175,7 +175,8 @@ void forward_ec_scheduler::ProcessBatch(bess::PacketBatch *batch){
 	  cudaMallocManaged(&pkts, bess::PacketBatch::kMaxBurst*bess::PacketBatch::kMaxBurst * sizeof(Pkt));
 	  cudaMallocManaged(&fs, bess::PacketBatch::kMaxBurst * sizeof(Fs));
 	  int i=0;
-	  for(i=0;i<32/*coordinator_actor_->active_flows_rrlist_.get_size()*/;){
+	  Pkt_reset(pkts,32*32);
+	  while(i<32/*coordinator_actor_->active_flows_rrlist_.get_size()*/){
 
 		  it_actor=coordinator_actor_->active_flows_rrlist_.rotate();
 		  if(it_actor->get_queue_ptr()->empty()){
