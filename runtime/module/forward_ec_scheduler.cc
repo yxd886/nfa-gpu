f7a2bbc65d085d725a8eac2d2fa7456a001aae08
xiaodongyi <409070169@qq.com>
2017-04-10 15:36:51 +0800

    put insert outside the function

--- runtime/module/forward_ec_scheduler.cc
+++ runtime/module/forward_ec_scheduler.cc
@@ -261,8 +261,8 @@ void forward_ec_scheduler::ProcessBatch(bess::PacketBatch *bat){
 				//		i+=bess::PacketBatch::kMaxBurst;
 				//	}
 
-					char* dst=coordinator_actor_->pkts[i+times*bess::PacketBatch::kMaxBurst].pkt;
-					char* src=it->head_data<char*>();
+					//char* dst=coordinator_actor_->pkts[i+times*bess::PacketBatch::kMaxBurst].pkt;
+					//char* src=it->head_data<char*>();
 					//memcpy(dst,src,it->total_len());
 
 					//Format(src,&(coordinator_actor_->pkts[i+times*bess::PacketBatch::kMaxBurst].headinfo));
