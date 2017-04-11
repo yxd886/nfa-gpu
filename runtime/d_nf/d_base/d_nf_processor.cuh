6c681d16612e4b64bc0b78c1526fc4d98175c958
xiaodongyi <409070169@qq.com>
2017-04-11 13:06:41 +0800

    change pkt format

--- runtime/d_nf/d_base/d_nf_processor.cuh
+++ runtime/d_nf/d_base/d_nf_processor.cuh
@@ -10,7 +10,7 @@
 
 extern "C"{
 
-void gpu_nf_process(Pkt* pkts,Fs* fs,uint64_t service_chain,int packet_num);
+void gpu_nf_process(Pkt* h_pkts,Fs* h_fs,uint64_t service_chain,int packet_num,int *h_flow_size,int* h_flow_pos);
 
 
 }
