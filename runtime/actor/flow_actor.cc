e3bec5bbd1ea75b3b8176da7a28d30ad1ffa73a7
xiaodongyi <409070169@qq.com>
2017-04-11 20:00:16 +0800

    change pkt format

--- runtime/actor/flow_actor.cc
+++ runtime/actor/flow_actor.cc
@@ -83,6 +83,7 @@ void flow_actor::handle_message(flow_actor_init_with_pkt_t,
                                                 static_cast<uint16_t>(flow_actor_messages::check_idle));
 */
   //pkt_que.clear();
+  pkt_que=std::queue();
   cdlist_head_init(&buffer_head_);
 }
 
