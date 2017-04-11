44860888078353a27912d36b315c4946196fc6f7
xiaodongyi <409070169@qq.com>
2017-04-11 19:51:11 +0800

    change pkt format

--- runtime/actor/flow_actor.cc
+++ runtime/actor/flow_actor.cc
@@ -314,7 +314,7 @@ void flow_actor::pkt_normal_nf_processing(bess::Packet* pkt){
   if(service_chain_length_!=0){
 
 	  pkt_que.push(pkt);
-	  LOG(INFO)<<"queue_size: "<<pkt_que.size();
+	  printf("queue_size: %d",pkt_que.size());
 
   }
   if(in_have_packet_rrlist!=1){
