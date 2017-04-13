583a6b0b0e2ec5efe25510c43b6c32e0496bde0a
xiaodongyi <409070169@qq.com>
2017-04-13 21:55:21 +0800

    change firewall

--- runtime/d_nf/d_pktcounter/d_pkt_counter_fs.cuh
+++ runtime/d_nf/d_pktcounter/d_pkt_counter_fs.cuh
@@ -4,7 +4,18 @@
 #include <cstdint>
 #include "../d_base/Pkt.h"
 struct d_pkt_counter_fs{
-  uint32_t counter;
+	uint32_t SrcIp;
+	uint32_t DstIp;
+	uint16_t SrcPort;
+	uint16_t DstPort;
+	uint8_t protocol;
+	time_t   CreatedTime;
+	time_t   RefreshTime;
+	int match_no;
+	int drop_no;
+	int pass_no;
+	bool current_pass;
+	int counter;
 };
 
 #endif
