9a3b77562888cc8e46f8ca9ed895c5b2701781ab
xiaodongyi <409070169@qq.com>
2017-04-14 19:52:11 +0800

    60

--- runtime/module/forward_ec_scheduler.cc
+++ runtime/module/forward_ec_scheduler.cc
@@ -230,7 +230,7 @@ void forward_ec_scheduler::ProcessBatch(bess::PacketBatch *bat){
 			    char* dst=coordinator_actor_->pkts[idx][ pkt_id].pkt;
 				char* src=dp_pkt_batch.pkts()[i]->head_data<char*>();
 				if(dst==NULL||src==NULL) continue;
-				rte_memcpy(dst,src,dp_pkt_batch.pkts()[i]->total_len()<PKT_SIZE?dp_pkt_batch.pkts()[i]->total_len():PKT_SIZE);
+				memcpy(dst,src,dp_pkt_batch.pkts()[i]->total_len()<PKT_SIZE?dp_pkt_batch.pkts()[i]->total_len():PKT_SIZE);
 				//Format(src,&(coordinator_actor_->pkts[idx][pkt_id].headinfo));
 
 				Fs_copyback(&(coordinator_actor_->fs[idx][flow_id[actor_id]]),(flow_actor*)coordinator_actor_->fs[idx][flow_id[actor_id]].ptr);
