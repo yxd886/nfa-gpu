a01427c69694636e249774bbdb8a2013aafaae39
xiaodongyi <409070169@qq.com>
2017-04-14 17:59:49 +0800

    60

--- runtime/module/forward_ec_scheduler.cc
+++ runtime/module/forward_ec_scheduler.cc
@@ -230,6 +230,7 @@ void forward_ec_scheduler::ProcessBatch(bess::PacketBatch *bat){
 				memcpy(dst,src,dp_pkt_batch.pkts()[i]->total_len()<PKT_SIZE?dp_pkt_batch.pkts()[i]->total_len():PKT_SIZE);
 				//Format(src,&(coordinator_actor_->pkts[idx][pkt_id].headinfo));
 				Fs_copy(&(coordinator_actor_->fs[idx][flow_id[*actor_ptr]]),*actor_ptr);
+				Fs_copyback(&(coordinator_actor_->fs[idx][flow_id[*actor_ptr]]),*actor_ptr);
 				//rte_memcpy(dp_pkt_batch.pkts()[i]->head_data(), &((*actor_ptr)->output_header_.ethh), sizeof(struct ether_hdr));
 				 gettimeofday(&insert_end,0);
 
@@ -323,7 +324,7 @@ void forward_ec_scheduler::ProcessBatch(bess::PacketBatch *bat){
 				 // flow_actor** actor_ptr=coordinator_actor_->actorid_htable_.Get(&(coordinator_actor_->fs[!idx][j].actor_id_64));
 				 // if(unlikely(actor_ptr==nullptr)) continue;
 				  flow_actor* actor=coordinator_actor_->fs[!idx][j].ptr;
-				  Fs_copyback(&(coordinator_actor_->fs[!idx][j]),actor);
+				//  Fs_copyback(&(coordinator_actor_->fs[!idx][j]),actor);
 				}
 
 			}else{
