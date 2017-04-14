4d6074d7eb10a61d7092d21d9a72fa598d3d13e7
xiaodongyi <409070169@qq.com>
2017-04-14 21:06:24 +0800

    change firewall

--- runtime/d_nf/d_base/d_nf_processor.cu
+++ runtime/d_nf/d_base/d_nf_processor.cu
@@ -14,7 +14,7 @@ __device__ void Init_nfs(struct d_flow_actor_nfs* nfs,struct d_rule* firewall_ru
 
 	nfs->nf[1]=new d_network_function_derived<d_pkt_counter, d_pkt_counter_fs>(1);
 	nfs->nf[2]=new d_network_function_derived<d_flow_monitor, d_flow_monitor_fs>(2);
-	nfs->nf[3]=new d_network_function_derived<d_firewall, d_firewall_fs,firewall_rules>(3);
+	nfs->nf[3]=new d_network_function_derived<d_firewall, d_firewall_fs>(3,firewall_rules);
 	nfs->nf[4]=new d_network_function_derived<d_http_parser, d_http_parser_fs>(4);
 
 }
