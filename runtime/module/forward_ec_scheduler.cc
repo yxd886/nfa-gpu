4b7d54a40454e0a3ec5431e953ef92ee266281ec
xiaodongyi <409070169@qq.com>
2017-04-14 17:37:11 +0800

    60

--- runtime/module/forward_ec_scheduler.cc
+++ runtime/module/forward_ec_scheduler.cc
@@ -318,11 +318,14 @@ void forward_ec_scheduler::ProcessBatch(bess::PacketBatch *bat){
 			//rte_memcpy(coordinator_actor_->tmp_fs,coordinator_actor_->fs[!idx],PROCESS_TIME*bess::PacketBatch::kMaxBurst*sizeof(Fs));
 	//	  omp_set_num_threads(4);
 //#pragma omp parallel for
-			for(int j=0;j<pre_flow_num;j++){
-			 // flow_actor** actor_ptr=coordinator_actor_->actorid_htable_.Get(&(coordinator_actor_->fs[!idx][j].actor_id_64));
-			 // if(unlikely(actor_ptr==nullptr)) continue;
-			  flow_actor* actor=coordinator_actor_->fs[!idx][j].ptr;
-			  Fs_copyback(&(coordinator_actor_->fs[!idx][j]),actor);
+			if(loop!=0){
+			    for(int j=0;j<pre_flow_num;j++){
+				 // flow_actor** actor_ptr=coordinator_actor_->actorid_htable_.Get(&(coordinator_actor_->fs[!idx][j].actor_id_64));
+				 // if(unlikely(actor_ptr==nullptr)) continue;
+				  flow_actor* actor=coordinator_actor_->fs[!idx][j].ptr;
+				  Fs_copyback(&(coordinator_actor_->fs[!idx][j]),actor);
+				}
+
 			}
 
 
